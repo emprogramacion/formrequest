0f6b0a54acf05e2c29597c707fccc801a4773505
Erasmo Martinez <em.programacion14@gmail.com>
2022-09-03 18:58:07 -0500

    Validaciones en un archivo aparte del controlador (PostRequests)

---
+++ app/Http/Requests/PostRequest.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class PostRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true; //Acá lo pasamos a true para que lo valide.
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'name' => 'required'
+        ];
+    }
+}
--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -81,18 +81,15 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Form Request
                 </div>
 
                 <div class="links">
-                    <a href="https://laravel.com/docs">Docs</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://blog.laravel.com">Blog</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://vapor.laravel.com">Vapor</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
+                    <form action="{{ route('posts.store') }}" method="POST" >
+                        @csrf <!-- Método que protege mi sistema, indicándole a Laravel que el formulario es de nuestro proyecto-->
+                        <input type="text" name="title">
+                        <input type="submit" value="Enviar">
+                    </form>
                 </div>
             </div>
         </div>
